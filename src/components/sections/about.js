6df79a9a83b65f88546544f5e4d3796eed1363c2
Yoandy Vargas <yoandy.vargas@icloud.com>
2021-08-04 00:23:05 -0400

    Adds about me content

---
+++ src/components/sections/about.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const AboutContainer = styled.div`
+  max-width: 760px;
+  padding: 0 2rem 4rem 2rem;
+  margin: 0 auto;
+  &::after {
+    display: block;
+    content: '';
+    background: var(--main-text-color);
+    opacity: 0.125;
+    width: 100%;
+    height: 2px;
+    margin-top: 4rem;
+  }
+  h2 {
+    font-size: 2rem;
+    margin-bottom: 1rem;
+  }
+  p {
+    line-height: 1.5;
+  }
+`
+
+const About = () => {
+  return (
+    <AboutContainer>
+      <h2>My Story</h2>
+      <p>
+      Hey there! 👋🏻 My name is Yoandy (YO-AHN-DEE), and I love the coming together of all things design and technology. 
+      I have always gravitated towards tinkering ever since I got my first PC in 1998, but it wasn't until 2017 while working at Apple that I would discover my passion for programming. 
+      One day at work, I participated in a Today @ Apple coding session – and I never looked back. 
+      </p>
+      <br/>
+      <p>
+      In December 2020, I became the first university graduate in my family. In January 2021, I went on a six-month career experience with Apple as a Web Designer and Developer. It's been a great ride so far! 👨🏻‍💻
+      </p>
+    </AboutContainer>
+  )
+}
+
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from '../components/layout'
 import Hero from '../components/sections/hero'
-import About from '../components/about'
+import About from '../components/sections/about'
 import Work from '../components/sections/work'
 
 const IndexPage = () => {
