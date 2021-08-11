3dc6751b92ef446d5ed52e9c0abbbf919ced50d5
Yoandy Vargas <yoandy.vargas@icloud.com>
2021-08-11 13:35:14 -0400

    Adds more about info

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -15,15 +15,36 @@ const AboutContainer = styled.section`
     color: var(--accent-color);
     text-transform: uppercase;
   }
+  .about__tech {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
 `
 
+const TechButton = styled.div`
+  background: var(--accent-bg-color);
+  width: max-content;
+  padding: 0.5rem 1rem;
+  border-radius: 8px;
+  font-weight: bold;
+  margin: 0.25rem;
+`
+  
+
 const About = () => {
+
+  const techArray = ['JavaScript ES6+', 'React', 'Gatsby', 'GraphQL', 'Stylus', 'Adobe XD']
+
   return (
     <AboutContainer>
       <h2>About Me</h2>
-      <p>Hey there, my name is Yoandy <span>(yo-ahn-dee)</span>, and I enjoy all things design and technology. I have always loved building and learning the inner workings of things ever since I got my first PC in 1998 – but it wasn't until 2017 that I would discover my passion for web development. I participated in a coding educational session at work and I absolutely fell in ❤️.</p>
-      <p>A few years later, I would receive my B.A. in Digital Media - Web Design from the University of Central Florida while specializing in web-focused software engineering, design principles, human-computer interaction, and user experience research.</p>
-      <p>Here are a few technologies I've been working with recently:</p>
+      <p>Hey there, my name is Yoandy <span>(yo-ahn-dee)</span>, and I enjoy all things design and technology. I have always loved building and learning the inner workings of things ever since I got my first PC in 1998 – but it wasn't until 2017 that I would discover my passion for coding and the web. I participated in a coding educational session at work and I absolutely fell in ❤️.</p>
+      <p>A few years later, I would receive my B.A. in Digital Media - Web Design from the University of Central Florida while specializing in web-focused software engineering, design principles, human-computer interaction, and UX research.</p>
+      <p>Here are some technologies I'm currently working with:</p>
+      <div className='about__tech'>
+        {techArray.map((item) => <TechButton>{item}</TechButton>)}
+      </div>
     </AboutContainer>
   )
 }
