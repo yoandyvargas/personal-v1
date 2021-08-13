504013c2d543aaddb698cac323798978d07f83b1
Yoandy Vargas <yoandy.vargas@icloud.com>
2021-08-13 12:06:16 -0400

    Implements icon components

--- src/components/index.js
+++ src/components/index.js
@@ -14,7 +14,11 @@ export { default as Header } from "./header";
 export { default as Footer } from "./footer";
 export { default as Seo } from "./seo";
 export { default as GlobalStyle } from "./globalStyle";
+export { default as Button } from "./button";
 
 //Icons
 export { default as GhIcon } from "../components/icons/github";
 export { default as ProdIcon } from "../components/icons/production";
+export { default as EmailIcon } from "../components/icons/email";
+export { default as LinkedinIcon } from "../components/icons/linkedin";
+export { default as ResumeIcon } from "../components/icons/resume";
